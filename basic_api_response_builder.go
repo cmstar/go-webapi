2396353041e158a072a337af2c60118d3fdba07a
cmstar <cosmo_sei@hotmail.com>
2022-04-21 15:05:06 +0800

    现在在返回请求前，可以正确识别被包装（Wrap）的 error 的类型了。

--- basic_api_response_builder.go
+++ basic_api_response_builder.go
@@ -1,6 +1,11 @@
 package webapi
 
-import "github.com/cmstar/go-errx"
+import (
+	"errors"
+	"reflect"
+
+	"github.com/cmstar/go-errx"
+)
 
 // basicApiResponseBuilder 提供 ApiResponseBuilder 的标准实现。
 type basicApiResponseBuilder struct {
@@ -25,17 +30,46 @@ func (r *basicApiResponseBuilder) BuildResponse(state *ApiState) {
 
 	// ApiResponse 内容是返回给请求者的，不应该暴露内部细节，只有 BizError 是和具体业务高度关联的，
 	// 可以给出具体信息；对于其他错误，只给一个笼统的信息。
-	switch v := state.Error.(type) {
-	case errx.BizError:
-		resp.Code = v.Code()
-		resp.Message = v.Message()
+	typBizError := reflect.TypeOf((*errx.BizError)(nil)).Elem()
+	if err := r.toErrorType(state.Error, typBizError); err != nil {
+		e := err.(errx.BizError)
+		resp.Code = e.Code()
+		resp.Message = e.Message()
+		return
+	}
 
-	case BadRequestError:
+	typBadRequestError := reflect.TypeOf(BadRequestError{})
+	if err := r.toErrorType(state.Error, typBadRequestError); err != nil {
 		resp.Code = ErrorCodeBadRequest
 		resp.Message = "bad request"
+		return
+	}
 
-	default:
-		resp.Code = ErrorCodeInternalError
-		resp.Message = "internal error"
+	resp.Code = ErrorCodeInternalError
+	resp.Message = "internal error"
+}
+
+func (r *basicApiResponseBuilder) toErrorType(src error, typDest reflect.Type) error {
+	// 错误可能被封装，需要一层层解出来。
+	// 同时 errx.BizError/StackfulError 是接口，需要以接口方式判断。
+	isInterface := typDest.Kind() == reflect.Interface
+
+	for {
+		typSrc := reflect.TypeOf(src)
+
+		if isInterface {
+			if typSrc.Implements(typDest) {
+				return src
+			}
+		} else {
+			if typSrc == typDest {
+				return src
+			}
+		}
+
+		src = errors.Unwrap(src)
+		if src == nil {
+			return nil
+		}
 	}
 }
---
+++ basic_api_response_builder_test.go
@@ -0,0 +1,110 @@
+package webapi
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/cmstar/go-errx"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_basicApiResponseBuilder_BuildResponse(t *testing.T) {
+	b := NewBasicApiResponseBuilder()
+
+	t.Run("no-error", func(t *testing.T) {
+		state := &ApiState{
+			Data:  123,
+			Error: nil,
+		}
+		b.BuildResponse(state)
+		expect := ApiResponse{
+			Code:    0,
+			Message: "",
+			Data:    123,
+		}
+		assert.Equal(t, expect, *state.Response)
+	})
+
+	t.Run("biz", func(t *testing.T) {
+		state := &ApiState{
+			Data:  "d",
+			Error: errx.NewBizError(12, "m", nil),
+		}
+		b.BuildResponse(state)
+		expect := ApiResponse{
+			Code:    12,
+			Message: "m",
+			Data:    "d",
+		}
+		assert.Equal(t, expect, *state.Response)
+	})
+
+	t.Run("biz-wrap", func(t *testing.T) {
+		state := &ApiState{
+			Data:  "d",
+			Error: errx.Wrap("p1", fmt.Errorf("p2: %w", errx.NewBizError(123, "mm", nil))),
+		}
+		b.BuildResponse(state)
+		expect := ApiResponse{
+			Code:    123,
+			Message: "mm",
+			Data:    "d",
+		}
+		assert.Equal(t, expect, *state.Response)
+	})
+
+	t.Run("badrequest", func(t *testing.T) {
+		state := &ApiState{
+			Data:  "d",
+			Error: CreateBadRequestError(nil, nil, "x"),
+		}
+		b.BuildResponse(state)
+		expect := ApiResponse{
+			Code:    ErrorCodeBadRequest,
+			Message: "bad request",
+			Data:    "d",
+		}
+		assert.Equal(t, expect, *state.Response)
+	})
+
+	t.Run("badrequest-wrap-from-panic", func(t *testing.T) {
+		var err error
+		func() {
+			defer func() {
+				err = errx.PreserveRecover("gg", recover())
+			}()
+			panic(CreateBadRequestError(nil, nil, "x"))
+		}()
+
+		state := &ApiState{
+			Data:  "d",
+			Error: err,
+		}
+		b.BuildResponse(state)
+		expect := ApiResponse{
+			Code:    ErrorCodeBadRequest,
+			Message: "bad request",
+			Data:    "d",
+		}
+		assert.Equal(t, expect, *state.Response)
+	})
+
+	t.Run("other", func(t *testing.T) {
+		state := &ApiState{
+			Data:  nil,
+			Error: errors.New("gg"),
+		}
+		b.BuildResponse(state)
+		expect := ApiResponse{
+			Code:    ErrorCodeInternalError,
+			Message: "internal error",
+			Data:    nil,
+		}
+		assert.Equal(t, expect, *state.Response)
+	})
+
+	t.Run("other-wrap", func(t *testing.T) {
+
+	})
+}
