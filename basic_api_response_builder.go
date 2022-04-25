6b70d7b7739079a179ab16eaa954f3347ab875d6
cmstar <cosmo_sei@hotmail.com>
2022-04-25 19:02:40 +0800

    用标准库的 errors.As 简化了异常判定。

--- basic_api_response_builder.go
+++ basic_api_response_builder.go
@@ -2,7 +2,6 @@ package webapi
 
 import (
 	"errors"
-	"reflect"
 
 	"github.com/cmstar/go-errx"
 )
@@ -30,16 +29,15 @@ func (r *basicApiResponseBuilder) BuildResponse(state *ApiState) {
 
 	// ApiResponse 内容是返回给请求者的，不应该暴露内部细节，只有 BizError 是和具体业务高度关联的，
 	// 可以给出具体信息；对于其他错误，只给一个笼统的信息。
-	typBizError := reflect.TypeOf((*errx.BizError)(nil)).Elem()
-	if err := r.toErrorType(state.Error, typBizError); err != nil {
-		e := err.(errx.BizError)
-		resp.Code = e.Code()
-		resp.Message = e.Message()
+	var bizErr errx.BizError
+	if errors.As(state.Error, &bizErr) {
+		resp.Code = bizErr.Code()
+		resp.Message = bizErr.Message()
 		return
 	}
 
-	typBadRequestError := reflect.TypeOf(BadRequestError{})
-	if err := r.toErrorType(state.Error, typBadRequestError); err != nil {
+	var badRequestErr BadRequestError
+	if errors.As(state.Error, &badRequestErr) {
 		resp.Code = ErrorCodeBadRequest
 		resp.Message = "bad request"
 		return
@@ -47,29 +45,4 @@ func (r *basicApiResponseBuilder) BuildResponse(state *ApiState) {
 
 	resp.Code = ErrorCodeInternalError
 	resp.Message = "internal error"
-}
-
-func (r *basicApiResponseBuilder) toErrorType(src error, typDest reflect.Type) error {
-	// 错误可能被封装，需要一层层解出来。
-	// 同时 errx.BizError/StackfulError 是接口，需要以接口方式判断。
-	isInterface := typDest.Kind() == reflect.Interface
-
-	for {
-		typSrc := reflect.TypeOf(src)
-
-		if isInterface {
-			if typSrc.Implements(typDest) {
-				return src
-			}
-		} else {
-			if typSrc == typDest {
-				return src
-			}
-		}
-
-		src = errors.Unwrap(src)
-		if src == nil {
-			return nil
-		}
-	}
 }
