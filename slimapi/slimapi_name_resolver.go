2178859e1d8b75aec0dfee8de51519df98bf9284
cmstar <cosmo_sei@hotmail.com>
2022-04-27 16:54:52 +0800

    slimapi：优化 Content-Type 头的识别过程。

--- slimapi/slimapi_name_resolver.go
+++ slimapi/slimapi_name_resolver.go
@@ -61,8 +61,10 @@ func (d *slimApiNameResolver) FillMethod(state *webapi.ApiState) {
 		contentType := req.Header.Get(webapi.HttpHeaderContentType)
 
 		// Content-Type 可以被分号分隔，如 “text/html; charset=UTF-8”，我们只需要前面这段。
-		contentType = strings.Split(contentType, ";")[0]
-		contentType = strings.TrimSpace(contentType)
+		if idx := strings.Index(contentType, ";"); idx > 0 {
+			contentType = contentType[:idx]
+			contentType = strings.TrimSpace(contentType)
+		}
 
 		switch contentType {
 		case webapi.ContentTypeJson:
